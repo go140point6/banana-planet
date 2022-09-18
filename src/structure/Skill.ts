fb460a23c0fffb64e7955d109e2c76e5be72cb7a
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-09-17 19:13:28 -0700

    update

--- src/structure/Skill.ts
+++ src/structure/Skill.ts
@@ -85,7 +85,6 @@ export class Heal extends Skill {
 
   use(p1: Fighter, _p2: Fighter) {
     
-    console.log("MaxHP: " + maxHP); //debug
     console.log("Before: " + p1.hp);  //debug
     const healAmount = Math.ceil(p1.hp * 0.2); //orig
   
