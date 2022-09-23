7c22c57f7a1ae4b78198036824c52f22c4cc00bc
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-09-23 15:18:55 -0700

    diff

--- src/structure/Monster.ts
+++ src/structure/Monster.ts
@@ -68,12 +68,12 @@ export class Monster extends Fighter {
     }  
 
     this.monsterDiff = (this.attack + this.hp + (Math.round(this.armor * 1000)) + (Math.round(this.critChance * 100)) + (Math.round(this.critDamage * 100)));
-    console.log("MonsterDiff: " + this.monsterDiff);
+    //console.log("MonsterDiff: " + this.monsterDiff); //debug
     this.playerDiff = (player.attack + player.hp + (player.armor * 1000) + (player.critChance * 100) + (Math.round(player.critDamage * 100)));
-    console.log("PlayerDiff " + this.playerDiff);
+    //console.log("PlayerDiff " + this.playerDiff); //debug
     this.diff = parseFloat((this.playerDiff / this.monsterDiff).toFixed(2));
     if (this.diff > 1) {
-      console.log("Player is stronger!");
+      //console.log("Player is stronger!"); //debug
       if (this.diff >= 1.03) {
         this.relative = 5;
       } else {
@@ -81,7 +81,7 @@ export class Monster extends Fighter {
       }
     }
     if (this.diff < 1) {
-      console.log("Player is weaker!");
+      //console.log("Player is weaker!"); //debug
       if (this.diff >= 0.97) {
         this.relative = 2;
       } else {
@@ -89,7 +89,7 @@ export class Monster extends Fighter {
       }
     }
     if (this.diff == 1) {
-      console.log("Player and Monster are evenly matched");
+      //console.log("Player and Monster are evenly matched"); //debug
       this.relative = 3;
     }
     //console.log("Monster Attack: " + this.attack);
