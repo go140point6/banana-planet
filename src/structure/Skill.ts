c65d997c1e3cb33e61522db68c8d843321e06497
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-09-30 14:21:53 -0700

    boss

--- src/structure/Skill.ts
+++ src/structure/Skill.ts
@@ -203,7 +203,7 @@ export class Damage extends Skill {
   }
 
   close(p1: Fighter, _p2: Fighter) {
-    p1.critDamage /= 3;
+    p1.critDamage /= 1.5;
     //console.log("Done: " + p1.critDamage);
     }
   }
